73edc871a87cf6b25409b2ca9ea45e1668c33930
Damian Lewis <damian@damianlewis.co.uk>
2017-02-17 15:21:14 +0000

    Minor update to virtualhost file

--- vm-scripts/serve-apache.sh
+++ vm-scripts/serve-apache.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 block="<VirtualHost *:80>
-       ServerName $1
-       ServerAlias www.$1
-       DocumentRoot $2
+    ServerName $1
+    ServerAlias www.$1
+    DocumentRoot $2
 
-        <Directory $2>
-            AllowOverride All
-            Require all granted
-        </Directory>
+    <Directory $2>
+        AllowOverride All
+        Require all granted
+    </Directory>
 
-       ErrorLog \${APACHE_LOG_DIR}/error.log
-       CustomLog \${APACHE_LOG_DIR}/access.log combined
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>"
 
 echo "$block" > "/etc/apache2/sites-available/$1.conf"
